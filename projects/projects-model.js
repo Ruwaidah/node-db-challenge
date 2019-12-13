4f3516be7af5f1b6b1723ba8ac884184c27a06e2
ruwaidah <Ruwidah.riyadh@gmail.com>
2019-12-13 11:16:45 -0600

    make endpoints for add new task and get project by ID

--- projects/projects-model.js
+++ projects/projects-model.js
@@ -1,9 +1,31 @@
 module.exports = {
-  getAllProjects
+  getAllProjects,
+  getProjectById,
+  getAllTask,
+  addTask
 };
 
 const db = require("../db-config");
 
+// Get All Projects
 function getAllProjects() {
   return db("projects");
+}
+
+// Get Project By ID
+function getProjectById(id) {
+  return db("projects")
+    .where({ id })
+    .first();
+}
+
+// Get Tasks
+function getAllTask(id) {
+  console.log(id);
+  return db("tasks").where({ project_id: id });
+}
+
+// Add Task to Project
+function addTask(data) {
+  return db("tasks").insert(data);
 }
--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -4,10 +4,27 @@ router.use(express.json());
 
 const projects = require("./projects-model");
 
+// Get All Projects
 router.get("/", (req, res) => {
   projects.getAllProjects().then(projects => {
     res.status(200).json(projects);
   });
 });
 
+// Get Project By ID
+router.get("/:id", (req, res) => {
+  projects.getProjectById(req.params.id).then(project => {
+    projects.getAllTask(req.params.id).then(tasks => {
+      if (tasks.length > 0) project.task = tasks;
+      res.status(200).json(project);
+    });
+  });
+});
+
+// Add Task to Project
+router.post("/:id/tasks", (req, res) => {
+  projects.addTask(req.body).then(task => {
+    res.status(200).json(task);
+  });
+});
 module.exports = router;
